b64bc64fb413161add941139be0e07946a77127a
vitordias1006 <vitordias09532@gmail.com>
2026-03-23 09:55:33 -0300

    instalado o EF. Criado o DBContext com os DBSets e adicionado a conexão com o banco de dados Oracle

---
+++ CP1-Academia.Infraestructure/Persistence/AcademiaContext.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibrary1.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CP1_Academia.Infraestructure.Persistence;
+
+public class AcademiaContext : DbContext
+{
+    public AcademiaContext(DbContextOptions<AcademiaContext> optionsAcademia) : base(optionsAcademia)
+    {
+    }
+    
+    public DbSet<Aluno>  Alunos { get; set; }
+    
+    public DbSet<AulaExtra> AulaExtras { get; set; }
+    
+    public DbSet<FichaTreino> FichaTreinos { get; set; }
+    
+    public DbSet<Funcionario> Funcionarios { get; set; }
+    
+    public DbSet<Gerente> Gerentes { get; set; }
+
+    public DbSet<Instrutor> Instrutors { get; set; }
+
+    public DbSet<Localizacao> Localizacoes { get; set; }
+
+    public DbSet<Plano> Planos { get; set; }
+    
+    public DbSet<RedeAcademia> RedeAcademias { get; set; }
+    
+    public DbSet<UnidadeAcademia> UnidadeAcademias { get; set; }
+
+
+
+
+
+
+
+}
