3c501ff348cbf876a4ae1bfd34044841c273b61d
vitordias1006 <vitordias09532@gmail.com>
2026-04-08 21:03:41 -0300

    feat: Criando configuration de entidades com seus devidos relacionamentos

---
+++ CP1-Academia.Infraestructure/Persistence/Configurations/RedeAcademiaConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary1.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CP1_Academia.Infraestructure.Persistence.Configurations;
+
+public class RedeAcademiaConfiguration : IEntityTypeConfiguration<RedeAcademia>
+{
+    public void Configure(EntityTypeBuilder<RedeAcademia> builder)
+    {
+        builder.ToTable("RedeAcademia");
+        
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Nome);
+        builder.Property(x => x.QntdUnidades);
+        builder.Property(x => x.Cnpj);
+        builder.Property(x => x.DataFundacao);
+        
+        builder.HasMany(x => x.UnidadesAcademia)
+            .WithOne()
+            .HasForeignKey(u => u.RedeAcademiaId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
