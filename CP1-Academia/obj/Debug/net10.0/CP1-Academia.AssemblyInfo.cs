0e51ffcf844bbbfeb5e0ab6f431ee663299c67b8
vitordias1006 <vitordias09532@gmail.com>
2026-04-13 09:21:45 -0300

    Adicionando credenciais oracle, mesmo com erro

--- CP1-Academia/obj/Debug/net10.0/CP1-Academia.AssemblyInfo.cs
+++ CP1-Academia/obj/Debug/net10.0/CP1-Academia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CP1-Academia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de546edfde1a050200cfbc670e2b3ba81a06ad1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c501ff348cbf876a4ae1bfd34044841c273b61d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CP1-Academia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CP1-Academia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
